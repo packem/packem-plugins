fa9d423dc2d905520ea37ac3ae710a41798d10e3
bukharim96 <bukharim96@gmail.com>
2019-03-23 14:38:02 +0200

    dev-plugin | setup

---
+++ dev-plugin/README.md
@@ -0,0 +1,1 @@
+# packem-dev-plugin
---
+++ dev-plugin/bundleTemp.js
@@ -0,0 +1,44 @@
+const { readFileSync } = require("fs-extra");
+const path = require("path");
+
+const devRuntime = readFileSync(path.join(__dirname, "devRuntime.js"));
+
+module.exports = (initialBundleContent, devServerPort) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Packem | DevServer</title>
+</head>
+<body>
+<script id="packem-bundle">
+/*
+ * Bundled with Packem v0.1.0 (${new Date().toUTCString()})
+ * Mode: development
+ */
+;(function() {
+  var __packemModules = {};
+  ${initialBundleContent}
+  
+  var __packem = {
+    require: function(modID) {
+      var module = { exports : {} };
+
+      __packemModules[modID](__packem.require, module, module.exports);
+
+      return module.exports.default || module.exports;
+    },
+    reload: function() { __packem.require("_mod_root"); }
+  }
+
+  __packem.reload();
+  
+  var devServerPort = "${devServerPort}";
+
+  ${devRuntime}
+})();
+</script>
+</body>
+</html>
+`;
