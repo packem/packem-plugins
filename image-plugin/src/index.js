f9791b59d5ef0bdfa008a1f6b36e3d59dd05334c
Saddam M <alimohamuda80@gmail.com>
2019-03-25 00:20:22 +0200

    image-plugin, bundle-stats-plugin | JPEG & PNG support with ImageMin, setup

---
+++ image-plugin/src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2019 packem-image-plugin
+ */
+
+const { PackemPlugin } = require("packem");
+
+const imagemin = require("imagemin");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminPngquant = require("imagemin-pngquant");
+
+class PackemImagePlugin extends PackemPlugin {
+  onModuleBundle(mod) {
+    switch (mod.extension) {
+      // case "gif":
+      // case "svg":
+      case "jpg":
+      case "jpeg":
+      case "png":
+        const imagesOutputPath =
+          this.pluginConfig.extractAssetsDirectory || "./dist/img";
+
+        imagemin([mod.filename], imagesOutputPath, {
+          plugins: [
+            imageminJpegtran(this.pluginConfig.imageminOptions.jpeg || {}),
+            imageminPngquant(this.pluginConfig.imageminOptions.png || {})
+          ]
+        });
+
+        return `\n\n// Source: "${mod.filename}"
+this._mod_${mod.id} = function(require, module, exports) {
+  module.exports = "${mod.filename}";
+}`;
+    }
+  }
+}
+
+module.exports = PackemImagePlugin;
